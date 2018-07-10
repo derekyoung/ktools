2a3dd8e52e36b391593363c88f287dac4cff4879
Derek Young <dereky@Dereks-Mac-mini-3.local>
2018-07-10 10:21:37 -1000

    force file conditions

--- rezip.py
+++ rezip.py
@@ -10,10 +10,16 @@ for file in cdg_files:
     date = datetime.datetime.fromtimestamp(os.path.getctime(file))
     fn = str(file).replace('.cdg','')
 
-    print('creating archive ' + fn)
-    with zipfile.ZipFile(fn + '.zip', mode='w') as zf:
-        zf.write(fn + '.mp3',compress_type=zipfile.ZIP_DEFLATED)
-        zf.write(fn + '.cdg',compress_type=zipfile.ZIP_DEFLATED)
+    if not(os.path.isfile(fn + '.zip')):
+        print('creating archive')
+        with zipfile.ZipFile(fn + '.zip', mode='w') as zf:
+            if os.path.isfile(fn + '.mp3'):
+                zf.write(fn + '.mp3',compress_type=zipfile.ZIP_DEFLATED)
+                zf.write(fn + '.cdg',compress_type=zipfile.ZIP_DEFLATED)
+# we only like mp3s
+#            elif os.path.isfile(fn + '.mp2'):
+#                zf.write(fn + '.mp2',compress_type=zipfile.ZIP_DEFLATED)
+        print(fn)
 
     # reset creation and modification time
     os.system('SetFile -d "{}" "{}"'.format(date.strftime('%m/%d/%Y %H:%M:%S'), fn + '.zip'))
